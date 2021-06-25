9a832493ca9e7bb0d41a807a07421c45ae5ef9a4
unknown <moralkerim@gmail.com>
2021-06-25 14:43:29 +0300

    Fixed teta1 bias for other angles. Completely true angles. Will start GUI design.

--- Code/project.m
+++ Code/project.m
@@ -33,13 +33,6 @@ w4 = [1 0 0]';
 w5 = [0 1 0]';
 w6 = [1 0 0]';
 
-%Screw points
-% q1 = [0        0        0]';
-% q2 = [d1       0        a1]';
-% q3 = [d1       0        a1+a2]';
-% q4 = [d1+d2    0        a1+a2+a3]';
-% q5 = q4;
-% q6 = q4;
 
 %Screw points
 q1 = [0        0        0]';
@@ -69,12 +62,12 @@ tetd5=deg2rad(61.88);
 tetd6=deg2rad(-142.61);
 
 %Desired angles
-% tetd1=deg2rad(10);
-% tetd2=deg2rad(10);
-% tetd3=deg2rad(10);
-% tetd4=deg2rad(10);
-% tetd5=deg2rad(10);
-% tetd6=deg2rad(10);
+% tetd1=deg2rad(0);
+% tetd2=deg2rad(0);
+% tetd3=deg2rad(0);
+% tetd4=deg2rad(0);
+% tetd5=deg2rad(0);
+% tetd6=deg2rad(0);
 
 tetd = [tetd1 tetd2 tetd3 tetd4 tetd5 tetd6];
 
@@ -141,9 +134,8 @@ q3p = [0 0 a1+a2]';  q4p = [d2 0 a1+a2+a3]';
 q5p = q4p;           q6p = q4p;
 
 
-
-Tt = [1 0 0 -d1*cos(tetd1);
-      0 1 0 -d1*sin(tetd1);
+Tt = [1 0 0 -d1*cos(tetc1);
+      0 1 0 -d1*sin(tetc1);
       0 0 1 0;
       0 0 0 1];
 
@@ -151,27 +143,11 @@ Mp = [1 0 0 d2+d3;
       0 1 0 0; 
       0 0 1 a1+a2+a3; 
       0 0 0 1];
-  
-% q1p = Tt*hp(q1);        q4p = Tt*hp(q4);
-% q2p = Tt*hp(q2);        q5p = Tt*hp(q5);
-% q3p = Tt*hp(q3);        q6p = Tt*hp(q6);
-% 
-% q1p = q1p(1:3);         q4p = q4p(1:3);
-% q2p = q2p(1:3);         q5p = q5p(1:3);
-% q3p = q3p(1:3);         q6p = q6p(1:3);
-  
+
 %Calculate Teta3
 T1 = Tt * Td* inv(Mp);
 % T1 = Td* inv(M);
 p2 = T1 * hp(q4p);
-%p2 = expm(S1_skew * tetd1)*expm(S2_skew * tetd2)...
-%   * expm(S3_skew * tetd3) * hp(q4p);
-
-% r3 = [0 0 a1+a2]';
-% sigma = norm((p2)-hp(q2p));
-% [tet0, tet3_0] = pk3(w3,q4p,q2,r3,sigma);
-% theta3c_1 = tet0+tet3_0;
-% theta3c_2 = tet0-tet3_0;
 
 %Verify results
 sigma_2 = sqrt(p2(1)^2+p2(2)^2+(p2(3)-a1)^2);
@@ -204,32 +180,18 @@ S6_skew = [w6_skew v6; zeros(1,4)];
 
 
  T2 = expm(-S1_skew*tetc1)*  Tt * Td* inv(Mp);
-%  T2 = Tt * expm(-S1_skew*tetc1)* Td* inv(Mp)
-%T2 = expm(S2_skew*(tetd2))*expm(S3_skew*tetd3);
 
 q7_hp = expm(S3_skew*tetc3)*hp(q4p);
 p3 = T2*hp(q4p); %p3 hatalı olmalı
-%p3_2 = expm(S2_skew*tetd2)*q7_hp;
-%!!!! q4p S5 den etkileniyor!!!!
-%p3_3 = expm(S2_skew*tetd2)*expm(S3_skew*tetd3)...
-%    *expm(S4_skew*tetd4)*expm(S5_skew*tetd5)*expm(S6_skew*tetd6)*hp(q4p);
-%p3 = p3_2;
 p3x = p3(1); p3y = p3(2); p3z = p3(3);
 q7x = q7_hp(1); q7y = q7_hp(2); q7z = q7_hp(3);
 
 at1 = p3x*q7z - p3x*a1 - q7x*p3z + q7x*a1;
 at2 = q7x*p3x + q7z*p3z - q7z*a1 - p3z*a1 + a1^2;
 tetc2 = atan2(at1,at2);
-%tetc2 = deg2rad(-45.571);
-
-r2 = [0 0 a1]';
-%verify
-tetc2_2 = pk1(w2,q7_hp(1:3),p3(1:3),r2);
-%p3 veya q7 yanlış olabilir.
 
 %Solving teta4 and teta5
 T3 = expm(-S3_skew*tetc3)*expm(-S2_skew*tetc2)*T2;
-% T3 = Tt * expm(-S3_skew*tetc3)*expm(-S2_skew*tetc2)* expm(-S1_skew*tetc1)* Td* inv(Mp);
 q6pp = [0 0 a1+a2+a3]';
 p4 = T3*hp(q6pp);
 
@@ -242,28 +204,6 @@ tetc4_2 = atan2(-p4y,-(a1+a2+a3-p4z));
 tetc5_2 = atan2( sqrt(2*p4x*d2-p4x^2),d2-p4x);
 tetc5_1 = atan2(-sqrt(2*p4x*d2-p4x^2),d2-p4x);
 
-%Confirm calculations
-% r4 = [d2 0 a1+a2+a3]';
-% 
-% [alpha,beta,gama2,u,v] = pk2(w4, w5, q6pp, p4(1:3), r4); %Solve Paden-kahn subproblem2
-% gama_1 = sqrt(gama2); gama_2 = -1*sqrt(gama2);
-% 
-% z1 = alpha*w4+beta*w5+gama_1*cross(w4,w5);
-% z2 = alpha*w4+beta*w5+gama_2*cross(w4,w5);
-% 
-% c1 = z1+r4; c2 = z2+r4;
-% 
-% %Solve for theta5
-% theta5_2_2 = (pk1(w5, q6pp, c1,r4));
-% theta5_1_2 = (pk1(w5, q6pp, c2,r4));
-% 
-% %Solve for theta4
-% theta4_2_2 = (pk1(-w4, p4(1:3), c1,r4));
-% theta4_1_2 = (pk1(-w4, p4(1:3), c2,r4));
-
-
-%tetc4 = 0;
-%tetc5 = deg2rad(-105.65);
 tetc4 = tetc4_2;
 tetc5 = tetc5_2;
 
